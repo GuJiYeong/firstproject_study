6a9ec02061fd9c417546de094a1d86c747408f70
mina <rnwldud96@gmail.com>
2023-12-31 17:38:04 +0900

    Rest API

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO ARTICLE(id, title, content)
+VALUES (1, '11111', 'aaaaaa');
+INSERT INTO ARTICLE(id, title, content)
+VALUES (2, '22222', 'bbbbbb');
+INSERT INTO ARTICLE(id, title, content)
+VALUES (3, '33333', 'cccccc');
--- src/main/resources/data_sql.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO article(title, content) VALUES(1, '11111', 'aaaaaa');
-INSERT INTO article(title, content) VALUES(2, '22222', 'bbbbbb');
-INSERT INTO article(title, content) VALUES(3, '33333', 'cccccc');
